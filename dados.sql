a56507c8c521bb511aad3b49ecae16f08f62e47d
Thyeris <109691373+TH3ERIS45@users.noreply.github.com>
2024-12-20 17:13:34 -0300

    Update dados.sql

--- dados.sql
+++ dados.sql
@@ -1,7 +1,3 @@
 
         INSERT INTO usuarios (nome, email, senha, idade, sexo, altura, peso) 
-        VALUES ('Carla', 'carla2@hotmail.com', 'addffdafsfd', 14, 'Feminino', 165, 65);
-    
-        INSERT INTO usuarios (nome, email, senha, idade, sexo, altura, peso) 
-        VALUES ('Terreson', 'eduardothyeri@gmail.com', 'ThdfuDFUDUFSASDFSF', 15, 'Masculino', 183, 65);
-    
+        VALUES ('Teste', 'teste@hotmail.com', 'teste123', 14, 'Masculino', 165, 65);
